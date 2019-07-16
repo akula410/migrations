31273f7238b0f1bf8233490c7aa01e42f3821777
oleksandr.akulenko <akula410@gmail.com>
2019-07-16 09:17:33 +0300

    Alpha version 0.1

--- example/main.go
+++ example/main.go
@@ -3,13 +3,25 @@ package main
 import (
 	"github.com/akula410/migrations"
 	c "github.com/akula410/migrations/src"
-	"migrations/example/generate"
+	//"migrations/example/generate"
 )
 
+/**
+	example -method=init
+	uncomment
+		"migrations/example/generate"
+		config.SetMigrationList(generate.MigrateList)
+	go build
+	example -method=create
+	go build
+	example -method=up
+	example -method=down -step=1
+
+ */
 func main(){
 	config := c.Config
 	config.SetPackageFileMigration("migrations/example/migrations")
-	config.SetMigrationList(generate.MigrateList)
+	//config.SetMigrationList(generate.MigrateList)
 
 	Management := &migrations.Management{}
 	Management.SetConfig(config).Init()
